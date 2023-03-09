2d7d9a8d8c20dd3ea3dd8093d30799281c2ba860
Rob Canciello <robertcanciello@gmail.com>
2023-03-09 16:58:36 -0500

    Flower Test Added
    Flower Test added

---
+++ Assets/Scripts/GodOfferingLogic.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodOfferingLogic : MonoBehaviour
+{
+    private SceneManager _sceneManager;
+    public GameObject godParticles;
+    public GameObject newFlowerObj;
+    public Transform flowerLocPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sceneManager = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneManager>();
+        print("Found Manager");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (collision.gameObject.tag == "Flower")
+        {
+            //_sceneManager.keyPlaced = true;
+            print("I'm colliding with " + collision.gameObject.name);
+            godParticles.SetActive(true);
+            Destroy(collision.gameObject);
+            Instantiate(newFlowerObj, flowerLocPos.position, flowerLocPos.rotation);
+        }
+    }
+}
